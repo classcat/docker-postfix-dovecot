d2d208f93efc896859dd381852cacfe47abd579d
ClassCat <support@classcat.com>
2015-04-30 06:53:49 +0900

    Add assets/local.cf

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,27 @@
 FROM ubuntu:trusty
 MAINTAINER Masashi Okumura <masao@classcat.com>
 
-# docker run -d -p 25:25 -p 587:587 \
-#   -e hostname=hostname -e domainname=domainname \
-#   masao/postfix-sv
-# Set multiple user credentials: -e smtp_user=user1:pwd1,user2:pwd2,...,userN:pwdN
-
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update && apt-get -y install supervisor postfix sasl2-bin
+RUN locale-gen en_US.UTF-8
+ENV LANG       en_US.UTF-8
+ENV LC_ALL     en_US.UTF-8
+RUN dpkg-reconfigure locales
 
+RUN  bash -c 'debconf-set-selections <<< "postfix postfix/main_mailer_type string Internet site"' \
+  && bash -c 'debconf-set-selections <<< "postfix postfix/mailname string mail.example.com"'
+
+RUN apt-get update && apt-get -y install supervisor postfix sasl2-bin spamassassin spamc
+
+WORKDIR /opt
 ADD assets/cc-init.sh /opt/cc-init.sh
 
 ADD assets/supervisord.conf /etc/supervisor/supervisord.conf
 
-CMD /opt/cc-init.sh;/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+ADD assets/spamassassin /etc/default/spamassassin
 
-#ENTRYPOINT ["/opt/startup.sh"]
+ADD assets/local.cf /etc/spamassassin/local.cf
 
+EXPOSE 25 587
+
+CMD /opt/cc-init.sh; /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
--- assets/cc-init.sh
+++ assets/cc-init.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# masao
+########################################################################
+# ClassCat/Postfix Asset files
+# maintainer: Masashi Okumura < masao@classcat.com >
+########################################################################
+
+## last modified : 29-apr-15 ##
 
 ###############
 ### POSTFIX ###
+###############
+
 function proc_postfix_basic () {
   echo "$domainname" > /etc/mailname
 
   postconf -e myhostname=$hostname
   postconf -e mydestination=$domainname,$hostname,localhost.localdomain,localhost
 
-  # enable a submission port.
+  # enable a submission port. postconf should be used.
   sed -i -e "s/^#submission/submission/" /etc/postfix/master.cf 
 
   # See http://www.postfix.org/wip.html
@@ -18,11 +25,12 @@ function proc_postfix_basic () {
 }
 
 function add_accounts () {
-  echo $smtp_user | tr , \\n > /var/tmp/passwd
+  echo $users | tr , \\n > /var/tmp/users
   while IFS=':' read -r _user _id _pwd; do
-    useradd -u $_id -p $_pwd -s /sbin/nologin $_user
-  done < /var/tmp/passwd
-  rm /var/tmp/passwd
+    useradd -u $_id -s /usr/sbin/nologin $_user
+    echo -e "${_pwd}\n${_pwd}" | passwd $_user
+  done < /var/tmp/users
+  rm /var/tmp/users
 }
 
 function proc_postfix_smtp_auth () {
@@ -37,10 +45,11 @@ auxprop_plugin: sasldb
 mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
 EOF
 
-  echo $smtp_user | tr , \\n > /var/tmp/passwd
+  echo $users | tr , \\n > /var/tmp/users
   while IFS=':' read -r _user _id _pwd; do
     echo $_pwd | saslpasswd2 -p -c -u $domainname $_user
-  done < /var/tmp/passwd
+  done < /var/tmp/users
+  rm /var/tmp/users
 
   chown postfix.sasl /etc/sasldb2
 }
@@ -51,13 +60,40 @@ function proc_postfix () {
   proc_postfix_smtp_auth
 }
 
+
+####################
+### SPAMASSASSIN ###
+####################
+
+function proc_spamassassin () {
+  mkdir /var/log/spamassassin
+  chown debian-spamd:debian-spamd /var/log/spamassassin
+
+  #sed -i -e "s/^ENABLED\s*=\s*0/ENABLED=1/" /etc/default/spamassassin
+  #sed -i -e "s/^CRON\s*=\s*0/CRON=1/" /etc/default/spamassassin
+
+  # smtp       inet  n       -       n       -       -       smtpd
+  sed -i -e "s/^smtp\s*inet\s*n\s*\-\s*n\s*\-\s*\-\s*smtpd\s*/smtp       inet  n       -       n       -       -       smtpd -o content_filter=spamassassin/" /etc/postfix/master.cf
+
+
+  echo 'spamassassin unix -     n       n       -       -       pipe' >> /etc/postfix/master.cf
+  echo '    user=debian-spamd argv=/usr/bin/spamc -f -e'            >> /etc/postfix/master.cf
+  echo '    /usr/sbin/sendmail -oi -f ${sender} ${recipient}'       >> /etc/postfix/master.cf
+}
+
+
 ##################
 ### SUPERVISOR ###
+##################
 # See http://docs.docker.com/articles/using_supervisord/
+
 function proc_supervisor () {
   cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
 [supervisord]
 nodaemon=true
+
+[program:spamassassin]
+command=/opt/cc-spamassassin.sh
 
 [program:postfix]
 command=/opt/cc-postfix.sh
@@ -66,10 +102,18 @@ command=/opt/cc-postfix.sh
 command=/usr/sbin/rsyslogd -n -c3
 EOF
 
+  cat >> /opt/cc-spamassassin.sh <<EOF
+#!/bin/bash
+service spamassassin start
+tail -F /var/log/spamassassin/spamd.log
+EOF
+
+  chmod +x /opt/cc-spamassassin.sh
+
   cat >> /opt/cc-postfix.sh <<EOF
 #!/bin/bash
-service postfix start
-tail -f /var/log/mail.log
+/usr/sbin/postfix start
+tail -F /var/log/mail.log
 EOF
 
   chmod +x /opt/cc-postfix.sh
@@ -77,6 +121,9 @@ EOF
 
 
 proc_postfix
+proc_spamassassin
 proc_supervisor
+
+# /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
 exit 0
